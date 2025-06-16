7ac0dc7df1258f4431f3e735c5e18eec6cf0a78b
mcassemiro2025 <cassemiro.maria@escola.pr.gov.br>
2025-06-16 20:00:07 -0300

    16 junho

---
+++ script.js
@@ -0,0 +1,9 @@
+// Animação de rolagem suave para os links do menu
+document.querySelectorAll('nav ul li a').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
